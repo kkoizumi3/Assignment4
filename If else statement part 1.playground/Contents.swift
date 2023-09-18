2aefecb1cffe431dd5f19a2b6d74859d44bbc115
Kaci Koizumi <koizumi3@hawaii.edu>
2023-09-17 17:09:11 -1000

    completed parts 1-4
    still need to complete on your own sect

--- If else statement part 1.playground/Contents.swift
+++ If else statement part 1.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Part 2 Assign 4
+
+var goodGrade = "I've been doing all my work"
+
+//create first if statement
+//this is the conditional statement
+if goodGrade == "I've been doing all my homework"
+{
+    //if block
+    print("I should be getting an A at this point")
+}
+//part 3
+else
+{
+    //else block
+    print("I need to work harder in this class")
+}
+
+//part 4
+var gradeA = true
+
+if gradeA == true
+{
+    print("yuh")
+}
+else
+{
+    print("naur")
+}
